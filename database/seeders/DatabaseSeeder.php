6e408c95c508474cadcb994697ed2949945242c4
Vladislav <131362319+vladSHOKO@users.noreply.github.com>
2025-08-08 12:47:31 +0300

    Add task factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Task;
 use App\Models\Teacher;
 use App\Models\User;
 use Illuminate\Database\Seeder;
@@ -23,5 +24,7 @@ class DatabaseSeeder extends Seeder
         Teacher::factory()->create([
             'user_id' => $baseUser
         ]);
+
+        Task::factory()->createMany(10);
     }
 }
