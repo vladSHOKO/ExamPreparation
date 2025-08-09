96cc9a53e97fac51e0c40db2193804485f96d1a9
Vladislav <131362319+vladSHOKO@users.noreply.github.com>
2025-08-09 19:51:43 +0300

    Add additional files property for task

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -4,9 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Task extends Model
 {
     use HasFactory;
-    protected $fillable = ['description', 'answer', 'subject', 'type', 'additionalFiles'];
+    protected $fillable = ['description', 'answer', 'subject', 'type'];
+
+    public function additionalFiles(): hasMany
+    {
+        return $this->hasMany(AdditionalFiles::class);
+    }
 }
