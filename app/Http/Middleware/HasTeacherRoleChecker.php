63eca6894f50abbabd7c529f722af3e5d43ce9d7
Vladislav <131362319+vladSHOKO@users.noreply.github.com>
2025-08-09 16:07:35 +0300

    Change teacher role checker name

--- app/Http/Middleware/HasTeacherRoleChecker.php
+++ app/Http/Middleware/HasTeacherRoleChecker.php
@@ -6,7 +6,7 @@ use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
 
-class TeacherChecker
+class HasTeacherRoleChecker
 {
     /**
      * Handle an incoming request.
--- routes/auth.php
+++ routes/auth.php
@@ -2,13 +2,13 @@
 
 
 use App\Http\Controllers\AuthController;
-use App\Http\Middleware\TeacherChecker;
+use App\Http\Middleware\HasTeacherRoleChecker;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
 Route::post('/login', [AuthController::class, 'login'])->name('login.submit');
 
-Route::group(['middleware' => TeacherChecker::class], function () {
+Route::group(['middleware' => HasTeacherRoleChecker::class], function () {
     Route::get('/register', [AuthController::class, 'showRegisterForm'])->name('register');
     Route::post('/register', [AuthController::class, 'register'])->name('register.submit');
 });
