ad7055875fdd22d2d7e152bc8beb44716ca51e30
Vladislav <131362319+vladSHOKO@users.noreply.github.com>
2025-08-09 15:49:53 +0300

    Add factory use statement in student entit

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -2,12 +2,15 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Student extends Model
 {
+    use HasFactory;
+
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
