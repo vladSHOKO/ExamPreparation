ea8fd742af8321af0da2d8784ba361a0d9cd5988
Vladislav <131362319+vladSHOKO@users.noreply.github.com>
2025-08-19 20:02:38 +0300

    Add controller for chat messages

--- app/Http/Controllers/ChatMessagesController.php
+++ app/Http/Controllers/ChatMessagesController.php
@@ -3,17 +3,36 @@
 namespace App\Http\Controllers;
 
 use App\Models\ChatMessage;
+use App\Models\Task;
+use App\Models\TaskSession;
 use Illuminate\Http\Request;
 
 class ChatMessagesController extends Controller
 {
-    public function getMessages(Request $request, $sessionId)
+    public function getMessages(Request $request, $taskId)
     {
-        return ChatMessage::all()->where('task_session_id', $sessionId);
+        $task = Task::find($taskId);
+        $taskSessionId = TaskSession::query()->where('task_id', $task->id)->first()->id;
+
+        return ChatMessage::query()->where('task_session_id', $taskSessionId)->get();
     }
 
-    public function postMessage(Request $request, $sessionId)
+    public function postMessage(Request $request, $taskId)
     {
-        return true;
+        $data = $request->validate([
+            'content' => 'required|string',
+            'role' => 'required|string'
+        ]);
+        $user = $request->user();
+
+        $taskSession = TaskSession::query()->where('task_id', $taskId)->first();
+
+        $chatMessage = new ChatMessage();
+        $chatMessage->content = $data['content'];
+        $chatMessage->role = $data['role'];
+
+
+
+        return json_encode(['role' => 'assistant', 'content' => 'Тестовый ответ от сервера']);
     }
 }
