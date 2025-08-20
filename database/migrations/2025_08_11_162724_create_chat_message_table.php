f6daafe50fd160d3269b2555b51890433a32e27f
Vladislav <131362319+vladSHOKO@users.noreply.github.com>
2025-08-20 12:08:08 +0300

    Fix table name in chat messages migration

--- database/migrations/2025_08_11_162724_create_chat_message_table.php
+++ database/migrations/2025_08_11_162724_create_chat_message_table.php
@@ -26,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('chat_message');
+        Schema::dropIfExists('chat_messages');
     }
 };
