7457efa2b02be2915175ad018c3d0eef80810b7c
Vladislav <131362319+vladSHOKO@users.noreply.github.com>
2025-08-24 22:23:32 +0300

    Delete unnecessary route

--- routes/chat.php
+++ routes/chat.php
@@ -8,5 +8,3 @@ Route::middleware(['auth', IsUserBelongsToStudySession::class])->group(function 
     Route::get('/messages/get/{taskId}', [ChatMessagesController::class, 'getMessages'])->name('getMessages');
     Route::post('/message/post/{taskId}', [ChatMessagesController::class, 'postMessage'])->name('postMessage');
 });
-
-Route::get('/test/{taskId}', [ChatMessagesController::class, 'getMessages']);
